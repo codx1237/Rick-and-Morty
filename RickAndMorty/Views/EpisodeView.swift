48278e88a3775767f2df055fa42701afe9abbe48
FIRAT ÖREN <oren_2011@hotmail.com>
2024-09-04 19:26:42 +0300

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- RickAndMorty/Views/EpisodeView.swift
+++ RickAndMorty/Views/EpisodeView.swift
@@ -9,6 +9,30 @@ import SwiftUI
 
 struct EpisodeView: View {
     @StateObject var seasonsModel = SeasonsModel()
+    @State var tapped = [false,false,false,false]
+    @State var filteredSeasons: [Seasons] = []
+
+    func filterr(ss: String) {
+        filteredSeasons = []
+        for season in seasonsModel.seasons {
+            if season.episode.contains("\(ss)") {
+                filteredSeasons.append(season)
+            }
+        }
+    }
+
+    func seasonBackground(tapped: Bool) -> Color {
+        if tapped {
+            return Color("TabbarColor")
+        } else {
+            return Color("TabbarColor").opacity(0.5)
+        }
+    }
+
+    func resetTappedState(){
+        self.tapped = [false,false,false,false]
+    }
+
     var body: some View {
         VStack(alignment: .leading){
             ZStack(alignment:.bottomLeading){
@@ -44,14 +68,22 @@ struct EpisodeView: View {
             HStack{
                 ForEach(0 ..< 4) { item in
                     Button(action: {
-
+                        resetTappedState()
+                        tapped[item].toggle()
+                        if item == 0 {
+                            filterr(ss: "S01")
+                        } else if item == 1 {
+                            filterr(ss: "S02")
+                        } else {
+                            filteredSeasons = seasonsModel.seasons
+                        }
                     }, label: {
                         Text("Season \(item + 1)")
                             .padding(10)
                             .foregroundStyle(Color.white)
                             .font(.system(size: 12))
                             .bold()
-                            .background(Color("TabbarColor"))
+                            .background(seasonBackground(tapped: tapped[item]))
                             .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
                     })
                 }
@@ -59,7 +91,7 @@ struct EpisodeView: View {
 
             ScrollView {
                 VStack{
-                    ForEach(seasonsModel.seasons) { season in
+                    ForEach(filteredSeasons) { season in
                         HStack(spacing: 10){
                             Image("location")
                                 .resizable()
@@ -86,6 +118,7 @@ struct EpisodeView: View {
         }.ignoresSafeArea(edges:.top)
             .task {
                 await seasonsModel.fetchSeasons()
+                filteredSeasons = seasonsModel.seasons
             }
 
 
